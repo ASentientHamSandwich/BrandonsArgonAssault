654c5caa86f1bc14a2e67f857663f8d3d40d24bf
ASentientHamSandwich <rister1221@gmail.com>
2022-10-21 14:09:29 -0500

    Started working on player input script

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow);
 
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = .1f;
+        float newXPos = transform.localPosition.x + xOffset;
+
+        transform.localPosition = new Vector3(newXPos, transform.localPosition.y, transform.localPosition.z);
+
+        
+        
     }
 }
