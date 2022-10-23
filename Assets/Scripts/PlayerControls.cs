33b37c5f9c1182f79928329ab7727a33a37b03b2
ASentientHamSandwich <rister1221@gmail.com>
2022-10-22 19:31:03 -0500

    Worked on pitch (x rotation during y movement)

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -7,12 +7,27 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] float controlSpeed = 10f;
     [SerializeField] float xRange = 10f;
     [SerializeField] float yRange = 7f;
+
+    [SerializeField] float positionPitchFactor = -2f;
   
 
     // Update is called once per frame
     void Update()
     {
+        ProcessTranslation();
+        ProcessRotation();
+    }
 
+    void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor;
+        float yaw = 0f;
+        float roll = 0f;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void ProcessTranslation()
+    {
         float xThrow = Input.GetAxis("Horizontal");
         float yThrow = Input.GetAxis("Vertical");
 
@@ -26,8 +41,5 @@ public class PlayerControls : MonoBehaviour
         float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
-
-        
-        
     }
 }
